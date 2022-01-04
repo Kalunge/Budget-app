5dddb0fc362c0056c1f8c25ed959ed1f0dc505bf
kalunge <muthomititus+github@gmail.com>
2022-01-04 12:17:26 +0100

    Add splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  authenticated :user do
-    root to: 'groups#index', as: :authenticated_root
+
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root to: 'groups#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'splash#index', as: :unauthenticated_root
+    end
   end
   
-  devise_for :users
   resources :groups
   resources :entities
 end
