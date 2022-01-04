e24550eda3e9896e49620b5dfd15025297ee5f23
kalunge <muthomititus+github@gmail.com>
2022-01-04 10:33:01 +0100

    Add devise for Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'entities/new'
-  get 'entities/create'
-  get 'entities/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  authenticated :user do
+    root to: 'groups#index', as: :authenticated_root
+  end
+  
+  devise_for :users
   resources :groups
   resources :entities
 end
