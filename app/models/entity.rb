57b33e9d450f95656a40bf4eb976dc510e91e210
Titus Muthomi <muthomititus+github@gmail.com>
2022-01-04 12:35:34 +0300

    Revert "Ruby on Rails capstone project - Budget app: Authentication"

--- app/models/entity.rb
+++
@@ -1,5 +0,0 @@
-class Entity < ApplicationRecord
-
-  has_and_belongs_to_many :groups
-  belongs_to :user
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  authenticated :user do
-    root to: 'groups#index', as: :authenticated_root
-  end
-  
-  devise_for :users
-  resources :groups
-  resources :entities
 end
